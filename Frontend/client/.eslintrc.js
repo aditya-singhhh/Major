62ac441874b05043be8dd81dfdc44686b5c91200
aditya_singhhh <adityaanurag1969@gmail.com>
2024-12-30 03:02:42 +0530

    Frontend correction

--- Frontend/client/.eslintrc.js
+++ Frontend/client/.eslintrc.js
@@ -11,14 +11,15 @@ module.exports = {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
+  parser: '@babel/eslint-parser', // Updated parser
   parserOptions: {
+    requireConfigFile: false, // Skip requiring a Babel config file
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  parser: 'babel-eslint',
   plugins: [
     'react',
   ],
